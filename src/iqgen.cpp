23ad4ad9192f26b644ef1c9532c8c9aed3bc3016
jebbailey <baileyji@ucsb.edu>
2022-01-31 18:04:01 -0800

    add phase stream generation

--- src/iqgen.cpp
+++ src/iqgen.cpp
@@ -21,7 +21,7 @@ void iq_gen_noiq(hls::stream<iqstream_t> &out, ap_uint<27> max) {
 	}
 }
 
-void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::stream<istream_t> &q_out, ap_uint<27> max) {
+void iq_gen_nophase(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::stream<istream_t> &q_out, ap_uint<27> max) {
 #pragma HLS INTERFACE mode=ap_ctrl_none port=return
 #pragma HLS INTERFACE axis port=out register_mode=both register
 #pragma HLS INTERFACE axis port=i_out register_mode=both register
@@ -54,3 +54,50 @@ void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::st
 		q_out.write(tmp2);
 	}
 }
+
+void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::stream<istream_t> &q_out, hls::stream<pstream_t> &phase, ap_uint<27> max) {
+#pragma HLS INTERFACE mode=ap_ctrl_none port=return
+#pragma HLS INTERFACE axis port=out register_mode=both register
+#pragma HLS INTERFACE axis port=i_out register_mode=both register
+#pragma HLS INTERFACE axis port=q_out register_mode=both register
+#pragma HLS INTERFACE axis port=phase register_mode=both register
+#pragma HLS INTERFACE s_axilite port=max
+
+	outloop: for (unsigned int i=0; i<max;i+=2) {
+#pragma HLS PIPELINE II=2 REWIND
+		iqstream_t tmp;
+		istream_t tmp2;
+		pstream_t ptmp;
+
+		for (int k=0; k<8; k++) tmp.data.range((k+1)*32-1,k*32)=i*8+k;
+		tmp.user=(i&255);
+		tmp.last=false;
+		out.write(tmp);
+
+		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=i*4+k;
+		ptmp.user=(i&511);
+		ptmp.last=false;
+		phase.write(ptmp);
+
+		for (int k=0; k<8; k++) tmp2.data.range((k+1)*16-1,k*16)=tmp.data.range((k+1)*32-1-16,k*32);
+		tmp2.last=false;
+		i_out.write(tmp2);
+		q_out.write(tmp2);
+
+		for (int k=0; k<8; k++) tmp.data.range((k+1)*32-1,k*32)=(i+1)*8+k;
+		tmp.user=((i+1)&255);
+		tmp.last=tmp.user==255;
+		out.write(tmp);
+
+		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=(i+1)*4+k;
+		ptmp.user=((i+1)&511);
+		ptmp.last=ptmp.user=511;
+		phase.write(ptmp);
+
+		for (int k=0; k<8; k++) tmp2.data.range((k+1)*16-1,k*16)=tmp.data.range((k+1)*32-1-16,k*32);
+		tmp2.last=tmp.last;
+		i_out.write(tmp2);
+		q_out.write(tmp2);
+	}
+}
+
--- src/iqgen.hpp
+++ src/iqgen.hpp
@@ -4,8 +4,10 @@
 #include <ap_utils.h>
 typedef ap_axiu<256,8,0,0> iqstream_t;
 typedef ap_axiu<128,0,0,0> istream_t;
+typedef ap_axiu<64,16,0,0> pstream_t;
 
 void iq_gen(hls::stream<iqstream_t> &out, ap_uint<27> max);
 void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &iout, hls::stream<istream_t> &qout, ap_uint<27> max);
+void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &iout, hls::stream<istream_t> &qout, hls::stream<pstream_t> &phase, ap_uint<27> max);
 
 
