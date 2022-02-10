73fd0e22f2839adafe51d7a6e49915d1576ca87c
jebbailey <baileyji@ucsb.edu>
2022-02-09 22:55:51 -0800

    Fix Phase stream generation and change each group to sequential

--- src/iqgen.cpp
+++ src/iqgen.cpp
@@ -55,6 +55,9 @@ void iq_gen_nophase(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out,
 	}
 }
 
+// iq: 8 32 bit ints  0...7 8...15 ... up to 2^32-1 or max*8-1 last every 256th group user 0-255
+// i & q: 8 16 bit ints  0...7 8...15 ... up to 2^16-1 or max*8-1 last every 256th group user 0-255
+// phase: 4 16 bit ints  0...3 4...7 ... up to 2^16-1 or max*4-1 last every 512th group user 0-511
 void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::stream<istream_t> &q_out, hls::stream<pstream_t> &phase, ap_uint<27> max) {
 #pragma HLS INTERFACE mode=ap_ctrl_none port=return
 #pragma HLS INTERFACE axis port=out register_mode=both register
@@ -72,32 +75,36 @@ void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::st
 		for (int k=0; k<8; k++) tmp.data.range((k+1)*32-1,k*32)=i*8+k;
 		tmp.user=(i&255);
 		tmp.last=false;
-		out.write(tmp);
 
-		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=i*4+k;
+		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=i;//*4+k;
 		ptmp.user=(i&511);
 		ptmp.last=false;
-		phase.write(ptmp);
 
 		for (int k=0; k<8; k++) tmp2.data.range((k+1)*16-1,k*16)=tmp.data.range((k+1)*32-1-16,k*32);
 		tmp2.last=false;
+//		tmp2.user=tmp.user;
+
+		out.write(tmp);
 		i_out.write(tmp2);
 		q_out.write(tmp2);
+		phase.write(ptmp);
 
 		for (int k=0; k<8; k++) tmp.data.range((k+1)*32-1,k*32)=(i+1)*8+k;
 		tmp.user=((i+1)&255);
 		tmp.last=tmp.user==255;
-		out.write(tmp);
 
-		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=(i+1)*4+k;
+		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=i+1;//(i+1)*4+k;
 		ptmp.user=((i+1)&511);
-		ptmp.last=ptmp.user=511;
-		phase.write(ptmp);
+		ptmp.last=ptmp.user==511;
 
 		for (int k=0; k<8; k++) tmp2.data.range((k+1)*16-1,k*16)=tmp.data.range((k+1)*32-1-16,k*32);
 		tmp2.last=tmp.last;
+//		tmp2.user=tmp.user;
+
+		out.write(tmp);
 		i_out.write(tmp2);
 		q_out.write(tmp2);
+		phase.write(ptmp);
 	}
 }
 
--- src/tb.cpp
+++ src/tb.cpp
@@ -10,10 +10,21 @@ int main() {
 
 	unsigned int max=256*4;
 	hls::stream<iqstream_t> out;
-	iq_gen(out, max);
+	hls::stream<istream_t> i_out, q_out;
+	hls::stream<pstream_t> phase;
+	iq_gen(out, i_out, q_out, phase, max);
+
+	// iq: 8 32 bit ints  0...7 8...15 ... up to 2^32-1 or max*8-1 last every 256th group user 0-255
+	// i & q: 8 16 bit ints  0...7 8...15 ... up to 2^16-1 or max*8-1 last every 256th group user 0-255
+	// phase: 4 16 bit ints  0...3 4...7 ... up to 2^16-1 or max*4-1 last every 512th group user 0-511
+
 
 	for (int i=0;i<max; i++) {
 		iqstream_t d=out.read();
+		istream_t di=i_out.read();
+		istream_t dq=q_out.read();
+		pstream_t dp=phase.read();
+
 		for (int j=0; j<8; j++) {
 			fail|=(d.data.range((j+1)*32-1,j*32).to_uint()!=i*8+j);
 			if (fail) {
@@ -23,9 +34,41 @@ int main() {
 		}
 		if (fail) break;
 
+		for (int j=0; j<8; j++) {
+			fail|=(di.data.range((j+1)*16-1,j*16).to_uint()!=i*8+j);
+			if (fail) {
+				cout<<"failed,i&q i,j="<<i<<","<<j<<endl;
+				break;
+			} else if (j==0) cout<<d.data.range((j+1)*16-1,j*16).to_uint()<<endl;
+		}
+		if (fail) break;
+
+		for (int j=0; j<4; j++) {
+			fail|=(dp.data.range((j+1)*16-1,j*16).to_uint()!=i);//*4+j);
+			if (fail) {
+				cout<<"failed, phase i,j="<<i<<","<<j<<endl;
+				break;
+			} else if (j==0) cout<<dp.data.range((j+1)*16-1,j*16).to_uint()<<endl;
+		}
+		if (fail) break;
+
+
+		fail|= di.data!=dq.data || di.user!=dq.user || di.last!=dq.last;
+		if (fail) break;
+
 		fail|= d.last!=(i%256 == 255);
 		if (fail) {
 			cout<<"last error"<<endl;
+			break;
+		}
+		fail|= di.last!=(i%256 == 255);
+		if (fail) {
+			cout<<"i&q last error"<<endl;
+			break;
+		}
+		fail|= dp.last!=(i%512 == 511);
+		if (fail) {
+			cout<<"phase last error"<<endl;
 			break;
 		}
 
@@ -34,7 +77,18 @@ int main() {
 			cout<<"user error"<<endl;
 			break;
 		}
+//		fail|= di.user!=i%256;
+//		if (fail) {
+//			cout<<"i&q user error"<<d.user<<" "<<di.user<<" "<<dq.user<<endl;
+//			break;
+//		}
+		fail|= dp.user!=i%512;
+		if (fail) {
+			cout<<"phase user error"<<endl;
+			break;
+		}
 	}
+
 	fail|=!out.empty();
 	if (!out.empty()) cout<<"not empty error"<<endl;
 
