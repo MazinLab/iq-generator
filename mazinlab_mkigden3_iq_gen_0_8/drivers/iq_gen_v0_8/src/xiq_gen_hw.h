cf64e130d2da8b940d63388b2978c66ef54d19fb
jebbailey <baileyji@ucsb.edu>
2022-02-14 19:49:05 -0800

    Significantly reduce core size (factor of 2) II=1, slightly tighter timing constraints but still above 512. Works in multiples of 512 and starts/stops only on packet aligned boundaries.

---
+++ mazinlab_mkigden3_iq_gen_0_8/drivers/iq_gen_v0_8/src/xiq_gen_hw.h
@@ -0,0 +1,24 @@
+// ==============================================================
+// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2021.1 (64-bit)
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+// control
+// 0x00 : reserved
+// 0x04 : reserved
+// 0x08 : reserved
+// 0x0c : reserved
+// 0x10 : Data signal of max
+//        bit 19~0 - max[19:0] (Read/Write)
+//        others   - reserved
+// 0x14 : reserved
+// 0x18 : Data signal of run
+//        bit 0  - run[0] (Read/Write)
+//        others - reserved
+// 0x1c : reserved
+// (SC = Self Clear, COR = Clear on Read, TOW = Toggle on Write, COH = Clear on Handshake)
+
+#define XIQ_GEN_CONTROL_ADDR_MAX_DATA 0x10
+#define XIQ_GEN_CONTROL_BITS_MAX_DATA 20
+#define XIQ_GEN_CONTROL_ADDR_RUN_DATA 0x18
+#define XIQ_GEN_CONTROL_BITS_RUN_DATA 1
+
--- src/iqgen.cpp
+++ src/iqgen.cpp
@@ -1,9 +1,30 @@
 #include "iqgen.hpp"
+#include <assert.h>
+
+inline void adder(ap_uint<27> &x, ap_uint<27> _max, bool even) {
+//#pragma HLS PIPELINE II=1
+		if (x<_max-1-even) x+=2;
+		else x=even;
+}
+
+ap_uint<27> adder2(ap_uint<27> &xo, ap_uint<27> &xe, ap_uint<27> _max, bool odd) {
+#pragma HLS PIPELINE II=1
+	ap_uint<27> ret;
+	ret=odd ? xo:xe;
+	if (odd) {
+		adder(xo, _max, 0);
+	} else {
+		adder(xe, _max, 1);
+	}
+	return ret;
+}
+
+
 
 // iq: 8 32 bit ints  0...7 8...15 ... up to 2^32-1 or max*8-1 last every 256th group user 0-255
 // i & q: 8 16 bit ints  0...7 8...15 ... up to 2^16-1 or max*8-1 last every 256th group user 0-255
 // phase: 4 16 bit ints  0...3 4...7 ... up to 2^16-1 or max*4-1 last every 512th group user 0-511
-void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::stream<istream_t> &q_out, hls::stream<pstream_t> &phase, ap_uint<27> max, volatile bool run) {
+void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::stream<istream_t> &q_out, hls::stream<pstream_t> &phase, ap_uint<20> max, volatile bool run) {
 #pragma HLS INTERFACE mode=ap_ctrl_none port=return
 #pragma HLS INTERFACE axis port=out register_mode=both register
 #pragma HLS INTERFACE axis port=i_out register_mode=both register
@@ -12,51 +33,43 @@ void iq_gen(hls::stream<iqstream_t> &out, hls::stream<istream_t> &i_out, hls::st
 #pragma HLS INTERFACE s_axilite port=max
 #pragma HLS INTERFACE s_axilite port=run
 
-	ap_uint<26> i=0;
-	outloop: while (run) {
-#pragma HLS LOOP_TRIPCOUNT min=127 max=134217727 //(2**27-1)
-#pragma HLS PIPELINE II=2 REWIND
+	bool _run=run;
+	ap_uint<29> _max=max*512;
+	ap_uint<29> i=0;
+	outloop: while (_run) {
+#pragma HLS LOOP_TRIPCOUNT min=512 max=536870912 //(2**29-1)
+#pragma HLS PIPELINE II=1 REWIND
 		iqstream_t tmp;
 		istream_t tmp2;
 		pstream_t ptmp;
-		unsigned int cy=2*i.to_uint();
-		unsigned int cy1=cy+1;
+		unsigned int cy=i.to_uint();
+		unsigned int iq_user, phase_user;
+		iq_user=cy&255;
+		phase_user=cy&511;
 
 		for (int k=0; k<8; k++) tmp.data.range((k+1)*32-1,k*32)=cy*8+k;
-		tmp.user=(cy&255);
-		tmp.last=false;
+		tmp.user=iq_user;
+		tmp.last=iq_user==255;
 
 		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=cy;//*4+k;
-		ptmp.user=(cy&511);
-		ptmp.last=false;
-
-		for (int k=0; k<8; k++) tmp2.data.range((k+1)*16-1,k*16)=tmp.data.range((k+1)*32-1-16,k*32);
-		tmp2.last=false;
-//		tmp2.user=tmp.user;
-
-		out.write(tmp);
-		i_out.write(tmp2);
-		q_out.write(tmp2);
-		phase.write(ptmp);
-
-		for (int k=0; k<8; k++) tmp.data.range((k+1)*32-1,k*32)=cy1*8+k;
-		tmp.user=(cy1&255);
-		tmp.last=tmp.user==255;
-
-		for (int k=0; k<4; k++) ptmp.data.range((k+1)*16-1,k*16)=cy1;//(i+1)*4+k;
-		ptmp.user=(cy1&511);
-		ptmp.last=ptmp.user==511;
+		ptmp.user=phase_user;
+		ptmp.last=phase_user==511;
 
 		for (int k=0; k<8; k++) tmp2.data.range((k+1)*16-1,k*16)=tmp.data.range((k+1)*32-1-16,k*32);
 		tmp2.last=tmp.last;
-//		tmp2.user=tmp.user;
 
 		out.write(tmp);
 		i_out.write(tmp2);
 		q_out.write(tmp2);
 		phase.write(ptmp);
 
-		i = i<(max>>1) ? ap_uint<26>(i+1):ap_uint<26>(0);
+		if (i==_max) i=0;
+		else i++;
+		if (ptmp.last) _run=run;
 	}
+
+
 }
+
+
 
